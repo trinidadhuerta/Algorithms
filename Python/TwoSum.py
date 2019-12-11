cea2698a37386170916b31f1b11b7f73ef50324d
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2019-12-11 11:02:26 -0600

    Update TwoSum.py

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -12,5 +12,10 @@ return [0, 1].
 
 
 class Solution:
+
+    test_nums1 = [2,7,11,15] #target = 9
+    test_nums2 = [3,6,7,1,9,11,18,13,10] #target = 31
+    test_nums3 = [12,19,18,23,1,3,9,13,95] #target = 96
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         return 0
