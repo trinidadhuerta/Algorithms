a27ebbdd0919afe0ad35f5845dae0b5ded8c5eab
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2019-12-13 13:29:45 -0600

    Update TwoSum.py

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,3 +1,5 @@
+
+
 """
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -9,7 +11,8 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
-
+import sys
+from typing import List
 
 class Solution:
 
@@ -18,4 +21,37 @@ class Solution:
     test_nums3 = [12,19,18,23,1,3,9,13,95] #target = 96
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return 0
+
+        result = None
+
+        print(f"Searching for numbers that add up to {target} in the list {nums}")
+
+        """
+        BRUTE FORCE SOLUTION
+        for the brute force solution we can go through each element of the array and the compare it to the following elements
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (i != j) and (nums[i] + nums[j]) == target:
+                    print(f"two numbers found at indices {i} and {j}")
+                    print(f"the numbers are {nums[i]} and {nums[j]}")
+                    return [i,j]
+                else:
+                    continue
+        
+        """
+
+        """
+        O(N) SOLUTION
+        """
+        
+        return result
+
+    def printResult(self):
+        print(twoSum(test_nums1, 9))
+
+
+
+#test the code
+test = Solution()
+print(test.twoSum(test.test_nums1,9))
