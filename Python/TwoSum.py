bae1063afc9e6b2ceabca67eb12f3e3a0625f963
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2019-12-18 09:38:46 -0600

    Update TwoSum.py

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -10,9 +10,30 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+
+        --------> BRUTE FORCE SOLUTION <---------
+        for the brute force solution we can go through each element of the array and the compare it to the following elements
+         
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (i != j) and (nums[i] + nums[j]) == target:
+                    print(f"two numbers found at indices {i} and {j}")
+                    print(f"the numbers are {nums[i]} and {nums[j]}")
+                    return [i,j]
+                else:
+                    continue
 """
+
 import sys
 from typing import List
+
+"""
+        O(N) SOLUTION
+        Here we will use a dictionary to keep track of the values we have seen as well as the solution for that number
+        we will then check to see if the solution is in the dictionary and if it is return its index. 
+"""
 
 class Solution:
 
@@ -23,35 +44,16 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         result = None
-
+        solutions = {}
         print(f"Searching for numbers that add up to {target} in the list {nums}")
-
-        """
-        BRUTE FORCE SOLUTION
-        for the brute force solution we can go through each element of the array and the compare it to the following elements
+        
 
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if (i != j) and (nums[i] + nums[j]) == target:
-                    print(f"two numbers found at indices {i} and {j}")
-                    print(f"the numbers are {nums[i]} and {nums[j]}")
-                    return [i,j]
-                else:
-                    continue
-        
-        """
-
-        """
-        O(N) SOLUTION
-        """
-        
-        return result
-
-    def printResult(self):
-        print(twoSum(test_nums1, 9))
-
+            if (target - nums[i]) in solutions and solutions[target - nums[i]] != i:
+                return [solutions[target - nums[i]], i]
+            solutions[nums[i]] = i
 
 
 #test the code
 test = Solution()
-print(test.twoSum(test.test_nums1,9))
+print(f"The two numbers are in indices: {test.twoSum(test.test_nums2,31)}")
