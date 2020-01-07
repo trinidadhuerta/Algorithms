4312e980be9a3fb92bbeb3426abb22e644b65332
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2020-01-06 21:35:00 -0600

    correctly reverses positive numbers

--- Python/ReverseInteger.py
+++ Python/ReverseInteger.py
@@ -21,3 +21,19 @@ For the purpose of this problem, assume that your function returns 0 when the re
 
 class Solution:
     def reverse(self, x: int) -> int:
+        num_in = x
+        num_out = 0
+
+        while num_in != 0:
+            pop = num_in%10
+            num_in = int(num_in / 10)
+            num_out = int((num_out*10) + pop)
+            
+            print(f"number popped = {pop}")
+            print(f"next number to check = {num_in}")
+            print(f"current output = {num_out}")
+        
+        return 0
+
+print("Starting the check...")
+Solution().reverse(12345)
