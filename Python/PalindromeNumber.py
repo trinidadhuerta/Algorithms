b406d5b9c788b05f8c8f227487a87cc80fff56a4
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2020-01-06 20:49:48 -0600

    Create PalindromeNumber.py

---
+++ Python/PalindromeNumber.py
@@ -0,0 +1,20 @@
+"""
+Determine whether an integer is a palindrome. 
+
+An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+input: 121
+Output: True
+
+
+Example 2:
+input: -121
+Output: False
+
+
+Example 3:
+input: 10
+Output: False
+
+"""
