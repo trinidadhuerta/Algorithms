836c324a1f33174619bf0821fde7f6ee946c7c97
Trinidad Huerta <48325816+trinidadhuerta@users.noreply.github.com>
2020-01-14 15:17:23 -0600

    Update ValidParentheses.py

--- Python/ValidParentheses.py
+++ Python/ValidParentheses.py
@@ -28,6 +28,55 @@ Example 5:
 Input: "{[]}"
 Output: true
 
+
+The solution below uses a List to simulate the stack data structure. 
+
+if the parentheses that is read is an opening brace, then it places it into the stack
+if the parentheses that is a read is a closing brace, then
+    1. checks if the stack is currently empty
+    2. checks if the character is a valid character
+    3. cheks if the elemtn at the top of the stack is the opening brace for the character read in
+        4. if it is, then remove the last elemetn from the stack 
+        5. if it isnt then push the character at the top of the stack 
+
 """
 class Solution:
+
+
     def isValid(self, s: str) -> bool:
+        
+        map = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+
+        stack = []
+
+        #cannot be an odd number of characters in the string
+        if (len(s) % 2) > 0:
+            return False
+
+        
+        for char in s:
+            if stack and (char in map and stack[-1] == map[char]):
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return not stack
+
+
+#TESTING ALGORITHM
+test_string1 = "()" #true
+test_string2 = "()[]{}" #true
+test_string3 = "(]" #false
+test_string4 = "([)]" #false
+test_string5 = "{{}}" #true
+test_string6 = "({[({})]}{}())" # true
+
+print(f"Now testing the string: {test_string6}")
+print(Solution().isValid(test_string6))
+
+
+
